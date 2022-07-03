bf974c14638327ee90d6e9c0942ecb55ad085fcc
Trần Hữu Đan Trường <103228056+tranhuudantruong@users.noreply.github.com>
2022-07-03 10:59:47 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,57 +1,28 @@
-$(document).ready(function(){
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+const comment = document.querySelector('#list-comment')
+const commentItem = document.querySelectorAll('#list-comment .item')
+var translateY = 0
+var count = commentItem.length
 
-   var $sm = 480;
-   var $md = 768;
+next.addEventListener('click', function (event) {
+  event.preventDefault()
+  if (count == 1) {
+    // Xem hết bình luận
+    return false
+  }
+  translateY += -400
+  comment.style.transform = `translateY(${translateY}px)`
+  count--
+})
 
-   function resizeThis() {
-      $imgH = $('.middle img').width();
-      if ($(window).width() >= $sm) {
-         $('.left,.right,.section').css('height', $imgH);
-      } else {
-         $('.left,.right,.section').css('height', 'auto');
-      }
-   }
-
-   resizeThis();
-
-   $(window).resize(function(){
-      resizeThis();
-   });
-
-   $(window).scroll(function() {
-      $('.section').each(function(){
-         var $elementPos = $(this).offset().top;
-         var $scrollPos = $(window).scrollTop();
-
-         var $sectionH = $(this).height();
-         var $h = $(window).height();
-         var $sectionVert = (($h/2)-($sectionH/4));
-
-
-         if (($elementPos - $sectionVert) <= $scrollPos && ($elementPos - $sectionVert) + $sectionH > $scrollPos) {
-            $(this).addClass('animate');
-         } else {
-            $(this).removeClass('animate');
-         }
-      });
-   });
-
-   $('.btn-primary').click(function(){
-      alert('I lied');
-   });
-});
-
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+prev.addEventListener('click', function (event) {
+  event.preventDefault()
+  if (count == 3) {
+    // Xem hết bình luận
+    return false
+  }
+  translateY += 400
+  comment.style.transform = `translateY(${translateY}px)`
+  count++
+})
